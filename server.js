6b41e31531d0f9fd5696b933e127f59c93ac5317
ElieMaged <Narutossj23@yahoo.com>
2024-11-03 16:15:18 +0200

    q

--- server.js
+++ server.js
@@ -1,80 +1,32 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 3000;
 const path = require('path')
-const MongoClient = require('mongodb').MongoClient
-const uri = 'mongodb+srv://Harambessj:Harambe100@persona-quotes.3a9om.mongodb.net/?retryWrites=true&w=majority&appName=Persona-Quotes'
 
+const uri = "mongodb+srv://Harambessj:Harambe100@quotes.vuxgr.mongodb.net/?retryWrites=true&w=majority&appName=quotes";
 
-MongoClient.connect(uri, (err, client) => {
-    // ... do something here
-    console.log('Connected to Database')
-  })
 
   
 
-// Make sure you place this before your CRUD handlers!
-app.use(express.urlencoded({ extended: true }))
 
 
-const champs = {
-    'darius':{
-'name':'Darius',
-'faction':'Noxus',
-'ultimate':'Noxian Guillotine'
-    }, 
-    'sett':{
-        'name':'Sett',
-        'faction':'Ionia',
-        'ultimate':'The Showstopper'
-    },
+app.use(express.static(path.join(__dirname, 'public')))
 
-    'illaoi':{
-        'name':'Illaoi',
-        'faction':'Bilgewater',
-        'ultimate':'Leap of Faith'
-    }, 
-    
-    'samira':{
-        'name':'Samira',
-        'faction':'Noxus',
-        'ultimate':'Devil trigger'
-    },
-    
-    'xayah':{
-        'name':'Xayah',
-        'faction':'Ionia',
-        'ultimate':'Featherstorm'
-    },
-    
-    'rakan':{
-        'name':'Rakan',
-        'faction':'Ionia',
-        'ultimate':'The Quickness'
-    },
+app.listen(3000, () => console.log('Server is running on port 3000!'))
 
-    'unknown': {
-        'name':'unknown',
-        'faction':'unknown',
-        'ultimate':'unknown'
-    }
+app.get('/quotes', (req,res) => {
 
-}
-
-
-app.get('/', (req,res) => {
-res.sendFile(__dirname + '/index.html')
 })
 
 
-app.post('/quotes', (req,res) => {
-    console.log(req.body)
+
+app.get('/quotes/:name', (req,res) => {
+    console.log(req.params)
+    const name = req.params.name
+    res.json(posts.filter(prev => prev.name === name))
 })
 
 
 
 
-
-app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
-});
